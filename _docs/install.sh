c794f251ba8b6c4abf108876686de3383da0f389
Léo Derbois <contact@leoderbois.com>
2015-03-18 14:15:27 +0100

    update script

--- _docs/install.sh
+++ _docs/install.sh
@@ -8,7 +8,8 @@ cd Boba-Fett-Theme-For-Jekyll
 mv _configDemo.yml _config.yml
 
 mkdir assets
-
+mkdir css
+cp _docs/main.scss css/
 cp -r _docs/_demoposts _posts
 rm -rf assets
 cp -r _docs/assetsdemo assets
